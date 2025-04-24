e7c754c658c6b08e7c51941d2c757f4798bcaf28
Lukasz Migas <lukas.migas@yahoo.com>
2025-04-24 15:40:51 +0100

    cleanup

--- src/qtextraplot/__init__.py
+++ src/qtextraplot/__init__.py
@@ -5,6 +5,6 @@ from loguru import logger
 __version__ = "0.1.0"
 __author__ = "Lukasz G. Migas"
 __email__ = "lukas.migas@yahoo.com"
-__issue_url__ = "https://github.com/illumion-io/qtextraplot-issues/issues"
-__project_url__ = "https://ionglow.io/qtextraplot"
+__issue_url__ = "https://github.com/lukasz-migas/qtextraplot-issues/issues"
+__project_url__ = "https://lukasz-migas.com/qtextraplot"
 logger.disable("qtextraplot")
