f0b6f0c4f1cdef92cefb6f160aa223778e8f1218
Lukasz Migas <lukas.migas@yahoo.com>
2026-05-31 09:46:54 -0700

    Fixing test issues

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from napari.utils.colormaps import AVAILABLE_COLORMAPS
 
+from qtextraplot import widgets
 from qtextraplot.widgets import QtColormapButton
 
 
@@ -36,12 +37,43 @@ def test_colormap_button_custom_color_registers_colormap(qtbot) -> None:
     assert not button.icon().isNull()
 
 
-def test_colormap_button_popup_opens_combobox_list(qtbot) -> None:
+def test_colormap_button_popup_opens_combobox_list(qtbot, monkeypatch) -> None:
     """Left-click behavior should show the colormap option list directly."""
     button = QtColormapButton(colormap="magma")
     qtbot.addWidget(button)
 
+    class FakeColormapPopup:
+        def __init__(self, owner: QtColormapButton) -> None:
+            self.owner = owner
+            self.colormap = ""
+            self.was_moved = False
+            self.was_shown = False
+            self.was_raised = False
+            self.opened_list = False
+
+        def set_colormap(self, name: str) -> None:
+            self.colormap = name
+
+        def move(self, position) -> None:
+            self.was_moved = True
+
+        def show(self) -> None:
+            self.was_shown = True
+
+        def raise_(self) -> None:
+            self.was_raised = True
+
+        def show_colormap_list(self) -> None:
+            self.opened_list = True
+
+    monkeypatch.setattr(widgets, "_ColormapPopup", FakeColormapPopup)
+
     button.show_colormap_popup()
 
     assert button._popup is not None
-    qtbot.waitUntil(lambda: button._popup.combobox.view().isVisible())
+    qtbot.waitUntil(lambda: button._popup.opened_list)
+    assert button._popup.owner is button
+    assert button._popup.colormap == "magma"
+    assert button._popup.was_moved
+    assert button._popup.was_shown
+    assert button._popup.was_raised
