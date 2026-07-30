90d8f4a9020212ba1974233f01d3f9a59c424076
Lukasz Migas <lukas.migas@yahoo.com>
2026-07-30 09:23:35 +0100

    Fixing colorbar

--- src/qtextraplot/_napari/image/_vispy/__init__.py
+++ src/qtextraplot/_napari/image/_vispy/__init__.py
@@ -1,6 +1,9 @@
 from napari._vispy.utils.visual import overlay_to_visual
+from napari.components.overlays import ColorBarOverlay
+
+from qtextraplot._napari.image._vispy.color_bar import _SafeVispyColorBarOverlay
 
 
 def register_vispy_overlays():
     """Register vispy overlays."""
-    overlay_to_visual.update({})
+    overlay_to_visual.update({ColorBarOverlay: _SafeVispyColorBarOverlay})
